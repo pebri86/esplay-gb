45dcbe76a6bf4cc9a2677457294a789b978960d8
Fuji Pebri <pebri86@gmail.com>
2018-12-27 11:43:54 +0700

    delete build folder

--- build/gnuboy/component_project_vars.mk
+++
@@ -1,7 +0,0 @@
-# Automatically generated build file. Do not edit.
-COMPONENT_INCLUDES += $(PROJECT_PATH)/components/gnuboy
-COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/gnuboy -lgnuboy
-COMPONENT_LINKER_DEPS += 
-COMPONENT_SUBMODULES += 
-COMPONENT_LIBRARIES += gnuboy
-component-gnuboy-build: 
